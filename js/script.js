b8c691750db38e80f267037f15433d23853eb35f
PauloRicardoAb <pauloricardoab1@gmail.com>
2025-08-25 01:07:20 -0300

    dark mode

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const $html = document.querySelector('html');
+const $element = document.querySelector('#btnMode');
+// selecionando elementos do html e guardando numa variavel
+
+$element.addEventListener('click', () => {
+    // criando função, faz uma ação quando o usuario clicar no botao
+    $html.classList.toggle('darkMode')
+    // adiciona ou remove a tag darkMode
+})
